a72247849677b4b4d225feb16883c5bd205e0404
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-28 14:48:40 +0530

    Update

--- src/app/components/directives/built-in/built-in.component.ts
+++ src/app/components/directives/built-in/built-in.component.ts
@@ -9,9 +9,20 @@ export class BuiltInComponent implements OnInit {
 
   constructor() { }
   gradientToggle: boolean;
-
+  toggleButton:boolean;
+  messages:string[];
   ngOnInit(): void {
     this.gradientToggle = true;
+    this.toggleButton = true;
+    this.messages=[
+      'message 1',
+      'message 2',
+      'message 3',
+      'message 4',
+      'message 5',
+    ];
+
+
   }
 
 }
