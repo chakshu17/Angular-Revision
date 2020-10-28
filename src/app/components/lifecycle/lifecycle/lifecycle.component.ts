6b5254a44c2a3c065de81b8b62b11ff90874dfd9
Chakshu kishnani <chakshu05kishnani@gmail.com>
2020-10-28 10:07:12 +0530

    Update

--- src/app/components/lifecycle/lifecycle/lifecycle.component.ts
+++ src/app/components/lifecycle/lifecycle/lifecycle.component.ts
@@ -1,23 +1,54 @@
-import { Component, DoCheck, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnInit,
+} from '@angular/core';
 
 @Component({
   selector: 'app-lifecycle',
   templateUrl: './lifecycle.component.html',
-  styleUrls: ['./lifecycle.component.scss']
+  styleUrls: ['./lifecycle.component.scss'],
 })
-export class LifecycleComponent implements OnInit,DoCheck {
+export class LifecycleComponent
+  implements
+    OnInit,
+    DoCheck,
+    OnChanges,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked {
+  constructor() {}
+  counter: number = 0;
 
-  constructor() { }
-  counter:number=0
-  ngDoCheck(){
+  ngDoCheck() {
     console.log('Do Check');
-
+  }
+  ngOnChanges() {
+    console.log('Ng OnChanges');
   }
   ngOnInit(): void {
     console.log('ng OnInit');
   }
-  clickCounter(){
-      this.counter++
+  ngAfterContentInit() {
+    console.log('After COntent Init');
+  }
+  ngAfterContentChecked() {
+    console.log('After COntent Checked');
+  }
+  ngAfterViewInit() {
+    console.log('After View Init');
+  }
+  ngAfterViewChecked() {
+    console.log('After View Chekced');
   }
 
+  clickCounter() {
+    this.counter++;
+  }
 }
